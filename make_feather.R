76df88a24650932b82d4b634d122655cd7c137b3
Ian Curtis <icurt523@gmail.com>
2021-12-07 13:21:29 -0500

    add more data, update app, prepare for publishing

---
+++ make_feather.R
@@ -0,0 +1,18 @@
+library(feather)
+library(tidyverse)
+
+my_files <- list.files(path=here::here('original_data/'), 
+                       pattern="*.csv", full.names = TRUE)
+spotify <- read_csv(my_files)
+
+# Adds a column for just the year, the track duration in seconds, and removes duplicates
+spotify <- spotify %>% 
+  mutate(year = substr(release_date, 1, 4),
+         duration_s = as.duration(duration) / 1000) %>% 
+  distinct(track_uri, .keep_all = TRUE)
+
+# Remove empty column
+spotify <- spotify[-20]
+
+# Write feather file
+write_feather(spotify, 'project/spotify.feather')
